8a6480a94fc2e3af7fc87f618ba319a5a8327fb6
ludpot31415 <ludpot31415@gmail.com>
2018-03-08 09:00:24 +0100

    Add files via upload

--- Files/p5_mouseBall - Game/sketch.js
+++ Files/p5_mouseBall - Game/sketch.js
@@ -2,6 +2,9 @@ var balls = [];
 var enemy = [];
 var ms = (new Date).getTime();
 var song;
+var maxSpeed = 8;
+var score = 0;
+var osc;
 
 function preload() {
     //song = loadSound("music/space.mp3");
@@ -9,12 +12,17 @@ function preload() {
 
 function setup() {
     //preload();
+    osc = new p5.Oscillator();
+    osc.setType('sine');
+    osc.start();
+    osc.amp(5);
+
     createCanvas(window.innerWidth, window.innerHeight);
     background(0);
     stroke(255);
     fill(255);
     textAlign("center");
-    
+
     for(var i = 0; i < 1; i++){
         balls[i] = new Ball();
     }
@@ -26,19 +34,20 @@ function setup() {
 }
 
 function draw() {
-    background(10);
+    osc.freq(random(15,40));
+    background(random(40));
     fill(255);
     textSize(32);
     textFont("Courier New");
     var t = floor(((new Date).getTime()-ms)/1000);
-    text("0".repeat(5 - Number(t.toString().length)) + t + "s", width/2, 30);
-    
+    text("TIME: " + "0".repeat(6 - Number(t.toString().length)) + t + "s" + " | SCORE: " + "0".repeat(6 - Number(score.toString().length)) + score + " | SPEED: " + "0".repeat(3 - Number(maxSpeed.toString().length)) + maxSpeed, width/2, 30);
+
     for(var i = 0; i < balls.length; i++){
         balls[i].collision();
         balls[i].update();
         balls[i].show();
     }
-    
+
     for (var i = 0; i < enemy.length; i++) {
         if (enemy[i].pos.x < 0 || enemy[i].pos.x > width) {
             enemy.splice(i,1);
@@ -50,31 +59,33 @@ function draw() {
         }
     }
 
-    for(var i = 0; i < enemy.length; i++) {
+    for (var i = 0; i < enemy.length; i++) {
         enemy[i].update();
         enemy[i].show();
-        
+
     }
 }
 
-function Reset() {
+function Reset(inc) {
     enemy = [];
     ms = (new Date).getTime();
+    balls[0].size = 15;
+    maxSpeed += inc;
 }
 
 function Enemy() {
-    this.pos = createVector(random(50,width),random(50,height));
-    this.vel = createVector(floor(random(-8,8)),floor(random(-8,8)));
+    this.pos = createVector(random(width/2-50,width/2+50),random(height/2-50,height/2+50));
+    this.vel = createVector(floor(random(-maxSpeed,maxSpeed)),floor(random(-maxSpeed,maxSpeed)));
     this.acc = createVector(0,0);
-    this.size = random(8,26);
-    this.color = [random(100,220),0,random(100,220)]
+    this.size = random(8,balls[0].size + 15);
+    this.color = [30,random(100,220),random(100,220)]
 
     this.update = function() {
         this.pos.add(this.vel);
         this.vel.add(this.acc);
 
     }
-    
+
     this.show = function() {
         noStroke();
         fill(this.color[0],this.color[1],this.color[2]);
@@ -87,8 +98,8 @@ function Ball() {
     this.acc = createVector(0,0);
     this.vel = createVector(0,0);
     this.mouseDist = createVector(mouseX-this.pos.x, mouseY-this.pos.y);
-    this.size = 10;
-    
+    this.size = 15;
+
     this.collision = function() {
         for (i = 0; i < enemy.length; i++) {
             /*if (this.pos.x >= enemy[i].pos.x && this.pos.x <= enemy[i].pos.x+enemy[i].size) {
@@ -97,20 +108,29 @@ function Ball() {
                 }
             }*/
             if ((this.size/2 + enemy[i].size/2) >= (Math.sqrt((this.pos.x-enemy[i].pos.x)**2 + (this.pos.y-enemy[i].pos.y)**2))) {
-                Reset();
+                if (this.size >= enemy[i].size) {
+                    score += 10;
+                    this.size += 0.5;
+                    enemy.splice(i,1);
+                    if (this.size == 55) {
+                        Reset(5);
+                    }
+                } else {
+                    Reset(0);
+                }
             }
-            
+
         }
     }
-    
+
     this.update = function() {
         this.pos.add(this.vel);
         this.vel.add(this.acc);
         this.mouseDist = createVector(mouseX-this.pos.x, mouseY-this.pos.y);
-        
-        this.vel = this.mouseDist.div(10);
+
+        this.vel = this.mouseDist.div(5);
     }
-    
+
     this.show = function() {
         fill(230);
         ellipse(this.pos.x, this.pos.y, this.size, this.size);
